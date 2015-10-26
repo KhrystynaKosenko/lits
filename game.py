62117b9e7e21ce0d716b56979eba1835001964a6
KhrystynaKosenko <khrystyna.kosenko@gmail.com>
2015-10-26 17:17:58 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,37 +1,34 @@
+import os, sys
+
 print "Привіт"
 print 'Зіграємо? так/ні'
 a1= raw_input(' ')
+#while True:
 if a1 == 'так':
-    print 'Вітаю у грі'
-    print '               '
-    print '***НЕНСІ ДРЮ***'
-    print '               '
+   	print 'Вітаю у грі'
+   	print '               '
+   	print '***НЕНСІ ДРЮ***'
+   	print '               '
 elif a1 == 'ні':
 	print 'Шкода. Гарного дня' 
 	sys.exit()
-		#break #можна додати ви впевнені
+	#break #можна додати ви впевнені
 else:
 	print "Дайте відповідь так або ні"
 	a1= raw_input()
-	while a1 != 'так' or 'ні':
-		a1 = raw_input()
-		if a1 == 'ні':
-			sys.exit()
-		else:
-			a1 == 'так'
-    		print 'Вітаю у грі'
-    		print '               '
-    		print '***НЕНСІ ДРЮ***'
-    		print '               '
-    		break
 name = raw_input('Як вас звати?')
-print 'Отже ' + str(name)+', уявіть що ви посеред гір і снігу, ви приїхали відпочити, і покататись на сноуборді, але в перший ж день з вашого готельного номеру було викрадено ваш сноуборд. І вам необхідно знайти його.'
+print 'Отже ' + str(name)+', уявіть що ви посеред гір і снігу,\n'
+print 'ви приїхали відпочити, і покататись на сноуборді, але в перший ж\n'
+print 'ж день з вашого готельного номеру було викрадено ваш сноуборд. '
+print 'І вам необхідно знайти його.'
+print '                  '
 print 'Зараз ви в кімнаті, оглядаєте речі, і бачите ключ на столі'
 keys= raw_input('Чи хочете ви їх взяти? так/ні')
 pocket=0
 if keys == 'так':
 	pocket = 1
 	print 'Чудово. Тепер ключі у вашій кишені'
+	print '            '
 elif keys == 'ні':
     pocket = 0
 	#print 'Ви обрали не забирати ключі'     
@@ -46,7 +43,9 @@ if answer_math == 'так':
 elif answer_math =='ні':
 	print 'Рухаємось далі'
 print '' 
-print 'Далі йдемо до холу, бачимо двох людей, хлопця в червоній куртці, що розгляда мапу і дівчину, котра читає'
+print 'Далі йдемо до холу, бачимо двох людей, хлопця в червоній куртці,'
+print 'що розгляда мапу і дівчину, котра читає'
+print''  
 print 'Вирішуємо поговорити з хлопцем, але він не розуміє української, тому переходимо на англійську'
 print 'Hi, I`m ' + str(name)+ ' And what`s your name?'
 print 'I`m Phillip'
@@ -99,7 +98,8 @@ for key in questions:
 			break
 	elif answer == '5':
 		print questions ['5']
-		print'Yes, there is a girl from Spain, she actually was here couple minutes ago, and there is a guy from Japan, but i don`t talk to him much'
+		print'Yes, there is a girl from Spain, she actually was here couple minutes ago,\n'
+		print 'and there is a guy from Japan, but i don`t talk to him much'
 		print 'Задати інше запитання? так/ні'
 		answer_end= raw_input()
 		if answer_end == 'так':
@@ -129,6 +129,7 @@ print ''
 print 'Тепер потрібно відчинити двері. Якщо у вас уже є ключ, то двері відчиняться автоматично'
 if pocket==1:
 	print'Двері відчинено'
+	print ''
 elif pocket==0:
 	print 'Верніться в кімнату і візьміть ключі.Забрали ключі, повертаємось до снігохода'
 else:
@@ -140,13 +141,17 @@ print 'Але, на жаль, на рецепції, не розмовляють
 print 'Hi, i need a map'
 print 'Sure, but firstly you need to solve one pazzle. Are you in? yes/no'
 answer_map = raw_input()
-if answer_map == 'yes':
-	print 'Great'
-elif answer_map== 'no':
-	print 'So no map for you. Game is over'
-	sys.exit()
-else:
-	print 'Please answer the question yes or no'
+while True:
+	if answer_map == 'yes':
+		print 'Great'
+		break
+	elif answer_map== 'no':
+		print 'So no map for you. Game is over'
+		sys.exit()
+	else:
+		print 'Please answer the question yes or no'
+		answer_map = raw_input()
+print '' 
 print'So the game is about guessing flags of different countries I`m going to say the country and you`ll need to say the colours on the flags. Do you understand? yes/no'
 answer_flag =raw_input()
 if answer_flag == 'yes':
@@ -155,58 +160,60 @@ elif answer_flag == 'no':
 	print 'When for you game is over'
 else:
 	'Answer please yes or no'
-print 'Which of these colours Spanish flag has?'
-ccolors = ['green', 'yellow','red', 'blue', 'white', 'black']
-x = raw_input()
-while x in colors: # в мене не виходить чомусь,щоб воно два кольори перевіряло
-	if x == 'red': #Цей і наступний блоки не по цих кольорах в мене не працюють і в мене не виходить як зробити,
-		print x    #так щоб воно перевіряло два кольори, якщо не вийде то я лишк цю частину щоб до кольорів просто вгадували один
-		break 
-	else:
-		print 'try again'
-		x = raw_input()
+print 'Spanish flag has 2 colors yellow and which else these colours Spanish flag has?'
+colors = ['green', 'yellow','red', 'blue', 'white', 'black']
+print colors
+print 'Choose the color'
+guess = raw_input()
+while True:
+  if guess == 'red':
+    print 'Great'
+    break
+  elif guess != 'red': 
+    print 'Try another color'
+    guess = raw_input()
+  else:
+    print 'Enter one of the colors'
 #if len(flag1) < 2:
 #	print'Type all colours'
 #else:
 #	bre
 print 'Correct, one more'
-print 'Which of these colours Nepal flag has?'
-colors = ['green', 'yellow','red', 'blue', 'white', 'black']
-correct = ['blue', 'red', 'white']
-print colors
-x= raw_input()
-while x in colors:
-	flag1=[]
-	if x == 'blue' or 'red' or 'white':
-		flag1.append(x)
-		print flag1
-		break
-	elif x != 'blue' or 'red' or 'white':
-		print'Ups, wrong'
-		#break 
-		#continue
-	else:
-		print'try again'
-		break
-print 'Correct, last one more'
-print 'Which of these colours Ukrainian flag has?'
+print ''
+print 'Nepali flag has 3 colors red, blue and?'
 colors = ['green', 'yellow','red', 'blue', 'white', 'black']
 print colors
-x= raw_input()
-while x in colors:
-	flag1=[]
-	x= raw_input()
-	if x == 'blue' or 'yellow':
-		flag1.append(x)
-		print flag1
-		break
-	else:
-		print'try again'
-		break
+print 'Choose the color'
+guess = raw_input()
+while True:
+  if guess == 'white':
+    print 'Great'
+    break
+  elif guess != 'white': 
+    print 'Try another color'
+    guess = raw_input()
+  else:
+    print 'Enter one of the colors'
+print 'Correct, last one'
+print ''
+print 'Brasilian flag has yellow and?'
+colors = ['green', 'yellow','red', 'blue', 'white', 'black']
+print colors
+print 'Choose the color'
+guess = raw_input()
+while True:
+  if guess == 'green':
+    print 'Great'
+    break
+  elif guess != 'green': 
+    print 'Try another color'
+    guess = raw_input()
+  else:
+    print 'Enter one of the colors'
 print 'Congrads, here is your map'
 print 'Тепер в нас є мапа, вирушаємо до дверей'
 print 'Тільки підійшовши до дверей ми бачимо, що хтось якраз виїхав. Ми вирішуємо їхати за ним'
-print 'Бачимо, що снігохідперед нами зупиняється перед невеликою хатиною, бачимо, що це Філіп, відвідувач з Перу, чекаємо поки він зникне за дверима'
+print 'Бачимо, що снігохід перед нами зупиняється перед невеликою хатиною, впізнаємо в ньому Філіпа'
 print 'Підійшовши до дверей бачимо замок з кодом, для того, щоб зайти нам необхідно підібрати код'
 print 'Оглянувшись навколо біля вікна бачимо видряпані цифри і номери кольорів, що їм відповідають'
 colors_code ={'1': 'white',
@@ -224,8 +231,10 @@ while code_hut != '151':
 	if code_hut == '151':
 		print 'Вітаю, ви вгадали'
 		break
-print 'Відкривши двері, ви бачите Філіпа і всі вкрадені речі'
-print 'Вітаю, ви знайшли свій сноуборд і всі інші вкрадені речі'
+print 'Відкривши двері, ви бачите Філіпа'
+print 'BUSTED'
+print ''
+print 'Вітаю, ви знайшли свій сноуборд і спіймали злодія'
 print ''
 
 
