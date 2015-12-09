93a2fcc62851d007c621ce6f9f54a75973185d01
KhrystynaKosenko <khrystyna.kosenko@gmail.com>
2015-12-09 03:11:18 +0200

    Update course_project.py

--- course_project.py
+++ course_project.py
@@ -3,37 +3,18 @@
 
 import nltk
 import os
-from nltk.corpus import PlaintextCorpusReader
+from nltk.corpus.reader import CategorizedPlaintextCorpusReader
+reader = CategorizedPlaintextCorpusReader('/home/khrystyna/python/course_project1/', r'author_.*\.txt', cat_pattern=r'author_(\w+)\.txt')
+print reader.categories()
+#print reader.fileids(categories=['murdoch'])
 
-#creating corpus with 3 texts in 3 files
-txt_fawles= os.path.abspath('/home/khrystyna/python/course_project/The_Magus.txt') 
-txt_hemingway= os.path.abspath('/home/khrystyna/python/course_project/.Hemingway_The_Old_Man_and_the_Sea.txt')
-txt_merdock= os.path.abspath('/home/khrystyna/python/course_project/Murdoch_Iris_The_Black_Prince.txt')
-corpus= [txt_fawles, txt_hemingway, txt_merdock]
-
-corpusdir = 'newcorpus/'
-if not os.path.isdir(corpusdir):
-    os.mkdir(corpusdir)
-
-filename = 0
-for text in corpus:
-    filename= filename + 1
-    open(corpusdir+str(filename)+'.txt','w): n
-   
-
-newcorpus = PlaintextCorpusReader('newcorpus/', '.*')
-
-
-
-import nltk
-from nltk.corpus import gutenberg
-nltk.corpus.gutenberg.fileids()
-for fileid in gutenberg.fileids():
-	num_chars = len(gutenberg.raw(fileid))
-	num_words = len(gutenberg.words(fileid))
-	num_sents = len(gutenberg.sents(fileid))
-	num_vocab = len(set([w.lower() for w in gutenberg.words(fileid)]))
+for fileids in reader.fileids():
+	num_chars = len(reader.raw(fileids))
+	num_words = len(reader.words(fileids))
+	num_sents = len(reader.sents(fileids))
+	num_vocab = len(set([w.lower() for w in reader.words(fileids)]))
 	average_word_length = int(num_chars/num_words)
 	average_sent_length = int(num_words/num_sents)
 	lexical_diversity = int(num_words/num_vocab)
-	print average_sent_length, average_word_length, lexical_diversity, fileid
+	print average_sent_length, average_word_length, lexical_diversity, fileids
+
